b1421cbad1695f2d0b745b1389aec5ad806894cd
Ajay Titus <ajaytitus1386@gmail.com>
2023-10-18 17:10:08 +0530

    feat: loading element

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero"
 import MasonGrid from "@/components/MasonGrid"
 import { fetchHomeImages } from "@/lib/data/images"
+import Image from "next/image"
 import React from "react"
 
 async function getHomeData() {
@@ -15,7 +16,22 @@ const Home = async () => {
     <div className="w-full h-full">
       <Hero />
 
-      {images.length > 0 ? <MasonGrid images={images} /> : <></>}
+      {images.length > 0 ? (
+        <MasonGrid images={images} />
+      ) : (
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            width={320}
+            height={320}
+            src={"/images/loading.gif"}
+            alt="Loading"
+            className="mix-blend-multiply"
+          />
+          <p className="text-text-light-400 dark:text-text-dark-400 font-bold text-xl">
+            Loading images...
+          </p>
+        </div>
+      )}
     </div>
   )
 }
