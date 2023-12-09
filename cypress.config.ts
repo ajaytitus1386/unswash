c5c9b7731735136736e7976a3ad8fe8f165531f7
Ajay Titus <ajaytitus1386@gmail.com>
2023-12-09 20:49:48 +0530

    feat: add cypress

---
+++ cypress.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "cypress"
+
+export default defineConfig({
+  e2e: {
+    env: {
+      "cypress-watch-and-reload": {
+        watch: ["app/**/*", "components/**/*", "lib/**/*"],
+      },
+    },
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      return require("cypress-watch-and-reload/plugins")(on, config)
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+})
