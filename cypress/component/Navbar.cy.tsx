0a78e29b73444a53171ad1e33ce8dba7ef73c9eb
ajaytitus1386 <ajaytitus1386@gmail.com>
2024-01-17 11:28:39 +0530

    chore: rmv mock router

--- cypress/component/Navbar.cy.tsx
+++ cypress/component/Navbar.cy.tsx
@@ -1,14 +1,11 @@
 /// <reference types="cypress" />
 
 import Navbar from "@/components/Navbar"
-import MockRouter from "../utils/router"
 
 describe("Navbar.cy.tsx", () => {
   it("mount Navbar", () => {
     cy.mount(
-      <MockRouter>
         <Navbar />
-      </MockRouter>
     )
   })
 })
